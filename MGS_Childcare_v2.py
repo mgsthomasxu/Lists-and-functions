b6371422f98d24883da85c5d4c542c0d4e474c57
mgsthomasxu <xut2@middleton.school.nz>
2024-02-14 14:19:32 +1300

    cai

--- MGS_Childcare_v2.py
+++ MGS_Childcare_v2.py
@@ -1,3 +1,25 @@
+def dropOff():
+    child_name = input("Enter the name of the child: ")
+    print(f"{child_name} has been added in.")
+
+
+def pickUp():
+    child_name = input("Enter the name of the child: ")
+    print(f"{child_name} has been added in.")
+
+
+def calcCost():
+    hour = float(input("How many hours will this child stay: "))
+    rate_per_hour = 12
+    total_cost = hour * rate_per_hour
+    print(f"The total cost for {hour} hours is ${total_cost:.2f}.")
+
+
+def printRoll():
+    child_name = input("Enter the name of the child: ")
+    print(f"Printing {child_name}...")
+
+
 choice = 0
 
 while choice != 5:
@@ -13,17 +35,17 @@ while choice != 5:
     print("5 Exit the system")
     print()
 
-choice = int(input("Enter your choice (number between 1 and 5): "))
-print()
+    choice = int(input("Enter your choice (number between 1 and 5): "))
+    print()
 
-if choice == 1:
-    dropOff()
-elif choice == 2:
-    pickUp()
-elif choice == 3:
-    calcCost()
-elif choice == 4:
-    printRoll()
-    print("Goodbye")
-else:
-    print("Please write the numbers within 1 to 5")
+    if choice == 1:
+        dropOff()
+    elif choice == 2:
+        pickUp()
+    elif choice == 3:
+        calcCost()
+    elif choice == 4:
+        printRoll()
+        print("Goodbye")
+    else:
+        print("Please write the numbers within 1 to 5")
